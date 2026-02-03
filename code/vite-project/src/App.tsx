e889458f043f82b5bd86190add5705cd72f0cfc9
Afsar Ahmed <afsar_zan@yahoo.com>
2026-02-03 08:17:59 -0800

    Chore: Cleanup files and basic layouting completed

--- code/vite-project/src/App.tsx
+++ code/vite-project/src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [ref, setRef] = useState('');
-  const [hyp, setHyp] = useState('');
+  const [ref, setRef] = useState<string>('');
+  const [hyp, setHyp] = useState<string>('');
+  const [result, setResult] = useState<number | null>(null);
   
   const calculateWer = () => {
-    
+   setResult(100);
   }
   return (
     <>
       <div className="wer-container">
+        <h1>Word Error Rate Calculator</h1>
         <div>
           <label htmlFor="reference">Reference</label>
           <input id="reference" type="text"  value={ref} onChange={(e) => setRef(e.target.value)}/>
@@ -20,6 +22,10 @@ function App() {
           <input id="hypothesis" type="text" value={hyp} onChange={(e) => setHyp(e.target.value)} />
         </div>
         <button onClick={calculateWer}>Compute WER</button>
+
+        <div className="result">
+          <strong>Result: WER: {result}</strong>
+        </div>    
       </div>
     </>
   )
