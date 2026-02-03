ebffe1bbb84e0a8280d38c6211b2a6d986fc2002
Afsar Ahmed <afsar_zan@yahoo.com>
2026-02-03 08:10:46 -0800

    Chore: UI - Layouting

--- code/vite-project/src/App.tsx
+++ code/vite-project/src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [ref, setRef] = useState('');
+  const [hyp, setHyp] = useState('');
+  
+  const calculateWer = () => {
+    
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wer-container">
+        <div>
+          <label htmlFor="reference">Reference</label>
+          <input id="reference" type="text"  value={ref} onChange={(e) => setRef(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor="hypothesis">Hypothesis</label>
+          <input id="hypothesis" type="text" value={hyp} onChange={(e) => setHyp(e.target.value)} />
+        </div>
+        <button onClick={calculateWer}>Compute WER</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
